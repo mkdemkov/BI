a3ca31ed08b3268be985a9c1c976ce4cb3ba9cdb
mkdemkov <mkdemkov@edu.hse.ru>
2024-03-19 15:40:05 +0300

    Initial commit

---
+++ backend/api/main.py
@@ -0,0 +1,16 @@
+# from fastapi import FastAPI
+
+# app = FastAPI()
+
+# data = [
+#     (1, "dadada", -16),
+#     (2, "wowo", 14),
+#     # more rows as needed
+# ]
+
+# headers = ["ID", "Description", "Value"]
+
+# @app.get('/my_data')
+# async def get_data():
+#     dict_data = [dict(zip(headers, row)) for row in data]
+#     return dict_data
